bbe7f558ec8fd7f736a6c79dfe67cbc3ad9faeba
Florian Vogel <florian.vogel@iteratec.de>
2017-10-29 20:22:18 +0100

    Fix path bug

---
+++ l3d-editor/backend/src/main/java/net/fvogel/controller/ImageController.java
@@ -0,0 +1,34 @@
+package net.fvogel.controller;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.NoSuchElementException;
+
+import javax.validation.ConstraintViolationException;
+
+import net.fvogel.model.Definition;
+import net.fvogel.persistence.DefinitionRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+import static org.springframework.web.bind.annotation.RequestMethod.GET;
+import static org.springframework.web.bind.annotation.RequestMethod.POST;
+
+@CrossOrigin(origins = "*")
+@RestController
+@RequestMapping("images")
+public class ImageController {
+
+    @RequestMapping(method = POST)
+    public void saveImages(@RequestParam("files") List<MultipartFile> files) {
+
+    }
+
+}
